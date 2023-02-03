640f3364dff26ed563bd1a8fa1a31fe95cc3bb31
nikolaitur <n.turovskyi@gmail.com>
2023-02-03 16:44:20 +0800

    :art: format files

--- remix.env.d.ts
+++ remix.env.d.ts
@@ -2,18 +2,18 @@
 /// <reference types="@shopify/remix-oxygen" />
 /// <reference types="@shopify/oxygen-workers-types" />
 
-import type {StorefrontClient} from '@shopify/hydrogen';
-import type {HydrogenSession} from '../server';
+import type { StorefrontClient } from '@shopify/hydrogen'
+import type { HydrogenSession } from '../server'
 
 /**
  * Declare expected Env parameter in fetch handler.
  */
 declare global {
   interface Env {
-    SESSION_SECRET: string;
-    SHOPIFY_STOREFRONT_API_PUBLIC_TOKEN: string;
-    SHOPIFY_STOREFRONT_API_VERSION: string;
-    SHOPIFY_STORE_DOMAIN: string;
+    SESSION_SECRET: string
+    SHOPIFY_STOREFRONT_API_PUBLIC_TOKEN: string
+    SHOPIFY_STOREFRONT_API_VERSION: string
+    SHOPIFY_STORE_DOMAIN: string
   }
 }
 
@@ -22,8 +22,8 @@ declare global {
  */
 declare module '@shopify/remix-oxygen' {
   export interface AppLoadContext {
-    session: HydrogenSession;
-    storefront: StorefrontClient['storefront'];
-    env: Env;
+    session: HydrogenSession
+    storefront: StorefrontClient['storefront']
+    env: Env
   }
 }
