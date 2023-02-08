eda62cb2a464488f7a32b997d232adb7e08c65ff
nikolaitur <n.turovskyi@gmail.com>
2023-02-07 19:53:44 -0800

    CHU-119 Update repo to match latest hydrogen build (#6)
    * Update dependencies
    
    * Changed server config
    
    * Changes to server entry file
    
    ---------
    
    Co-authored-by: Shaquille Galanto <shaquille@theformfactory.co>

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,18 +1,20 @@
 import { RemixServer } from '@remix-run/react'
 import type { EntryContext } from '@shopify/remix-oxygen'
-import { renderToString } from 'react-dom/server'
+import { renderToReadableStream } from 'react-dom/server'
 
-export default function handleRequest(
+export default async function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext,
 ) {
-  const markup = renderToString(<RemixServer context={remixContext} url={request.url} />)
+  const body = await renderToReadableStream(
+    <RemixServer context={remixContext} url={request.url} />,
+  )
 
   responseHeaders.set('Content-Type', 'text/html')
 
-  return new Response('<!DOCTYPE html>' + markup, {
+  return new Response(body, {
     status: responseStatusCode,
     headers: responseHeaders,
   })
