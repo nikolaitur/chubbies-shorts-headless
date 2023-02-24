66b669a824c6d48d704c2767236f50205e2f113e
nikolaitur <n.turovskyi@gmail.com>
2023-02-25 04:49:07 +0800

    Revert "[CHU-229]: Updated dependencies and icons" (#44)

--- app/sections/cart-slider/payment-information/payment-information.tsx
+++ app/sections/cart-slider/payment-information/payment-information.tsx
@@ -1,4 +1,4 @@
-import { LockIcon, RefreshIcon } from '@solo-brands/ui-library.ui.atomic.icon'
+import { LockIcon } from '@solo-brands/ui-library.ui.atomic.icon'
 import PaymentMethod from '@solo-brands/ui-library.ui.atomic.payment-method'
 import { forwardRef, HTMLAttributes, Ref } from 'react'
 import styles from './styles.module.css'
@@ -11,7 +11,7 @@ const PaymentInformation = ({ ...props }: PaymentInformationProps, ref: Ref<HTML
   return (
     <div className={styles.paymentInformation} ref={ref} {...props}>
       <div className={styles.title}>
-        <LockIcon size="md" />
+        <LockIcon size="xs" />
         <p>100% Secure Payment</p>
       </div>
       <div className={styles.methods}>
@@ -21,7 +21,7 @@ const PaymentInformation = ({ ...props }: PaymentInformationProps, ref: Ref<HTML
         <PaymentMethod variant="pay-pal" size="md" />
       </div>
       <div className={styles.exchange}>
-        <RefreshIcon size="md" />
+        <LockIcon size="xs" />
         <p>Free return and exchange within 90 days</p>
       </div>
     </div>
