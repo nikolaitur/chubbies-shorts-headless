13cc8519748e58d76100eea8fcb219de13898eab
nikolaitur <n.turovskyi@gmail.com>
2023-02-28 15:12:09 +0200

    CHU-285: [FOOTER][FUNC] Social links in the footer need to be connect… (#61)

--- app/sections/footer/footer-social-links/footer-social-links.tsx
+++ app/sections/footer/footer-social-links/footer-social-links.tsx
@@ -14,15 +14,15 @@ const FooterSocialLinks = ({ className }: { className?: string }) => (
     <h4 className={styles.title}>Follow Us</h4>
     <div className={styles.icons}>
       {/*@ts-expect-error TODO: Add href to icon*/}
-      <FacebookIcon size="sm" as="a" href="https://www.facebook.com/" />
+      <FacebookIcon size="sm" as="a" href="https://www.facebook.com/chubbies" />
       {/*@ts-expect-error TODO: Add href to icon*/}
-      <YoutubeIcon size="sm" as="a" href="https://www.youtube.com/" />
+      <YoutubeIcon size="sm" as="a" href="https://www.instagram.com/chubbies/" />
       {/*@ts-expect-error TODO: Add href to icon*/}
-      <InstagramIcon size="sm" as="a" href="https://www.instagram.com/" />
+      <InstagramIcon size="sm" as="a" href="https://www.youtube.com/chubbies" />
       {/*@ts-expect-error TODO: Add href to icon*/}
-      <SnapChatIcon size="sm" as="a" href="https://www.snapchat.com/" />
+      <TiktokIcon size="sm" as="a" href="https://www.tiktok.com/@chubbiesshorts" />
       {/*@ts-expect-error TODO: Add href to icon*/}
-      <TiktokIcon size="sm" as="a" href="https://www.tiktok.com/en/" />
+      <SnapChatIcon size="sm" as="a" href="https://www.chubbiesshorts.com/pages/snapchat" />
     </div>
   </div>
 )
